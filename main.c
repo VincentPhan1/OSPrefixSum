13579ba63499cb624758dc8656208a15aca21de0
VincentPhan1 <7005887+VincentPhan1@users.noreply.github.com>
2024-02-19 20:04:20 -0600

    Maybe it works?

--- main.c
+++ main.c
@@ -3,16 +3,22 @@
 #include <stdint.h>
 #include <math.h>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <stdbool.h>
 
 void Usage(char*);
 void readFile(FILE *fp, int *array);
 
 void prefixSum(int *array);
-void claculatePrefixSum(int *input, int *output, int step, int startIndex, int endIndex);
+void childSum(int *array, int startIndex, int chunkSize, int *prefixSums, int *flags, int steps, int identifier);
+void calculatePrefixSum(int *input, int *output, int step, int startIndex, int endIndex);
 
 
 uint16_t numElements;
 uint16_t numCores;
+
+int *array;
 
 int main(int argc, char** argv) {
     if(argc != 5) {
@@ -36,8 +42,16 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-
+    array = mmap(NULL, numElements * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    readFile(inputp, array);
     fclose(inputp);
+
+    prefixSum(array);
+
+    for(int i = 0; i < numElements; i++) {
+        printf("%d", array[i]);
+    }
+    munmap(array, numElements * sizeof(int));
     fclose(outputp);
 
     return EXIT_SUCCESS;
@@ -58,24 +72,60 @@ void calculatePrefixSum(int *input, int *output, int step, int startIndex, int e
 
 void prefixSum(int *array) {
     int numSteps = (int)log2(numElements);
+    int chunkSize = numElements/numCores;
+
+    int* prefixSums = mmap(NULL, numElements * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    int *flags = mmap(NULL, numCores * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    memset(prefixSums, 0, numElements * sizeof(int));
+    memset(flags, 0, numCores * sizeof(int));
+
 
     for(int i = 0; i < numCores; i++) {
         pid_t pid;
         pid = fork();
-
-        switch(pid) {
-            case -1:
-                perror("fork");
-                exit(EXIT_FAILURE);
-            case 0:
-
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if(pid == 0) {
+            int startIndex = i * chunkSize;
+            childSum(array, startIndex, chunkSize, prefixSums, flags, numSteps, i);
+            exit(EXIT_SUCCESS);
         }
     }
+
+    for (int i = 0; i < numCores; i++) {
+        while (flags[i] < numSteps) {} // Wait for other children to complete current step.
+    }
+
+    munmap(prefixSums, numElements * sizeof(int));
+    munmap(flags, numCores * sizeof(int));
 }
 
+void childSum(int *array, int startIndex, int chunkSize, int *prefixSums, int *flags, int steps, int identifier) {
+    int endIndex = (identifier == numCores - 1) ? numElements : startIndex + chunkSize;
 
+    for(int step = 1, prefixSteps = 1; step < steps; step++, prefixSteps *= 2) {
+        calculatePrefixSum(array, prefixSums, prefixSteps, startIndex, endIndex);
 
+        // oldest child rewrites input array to be the output array.
+        if (identifier == 0) {
+            for (int i = 1; i < numCores; i++) {
+                while (flags[i] < step) {} // Wait for other children to complete current step.
+            }
 
+            for (int i = 0; i < numElements; i++) {
+                array[i] = prefixSums[i];
+            }
+
+            flags[identifier] = step;   // oldest child is complete with everything.
+        }
+
+        else {
+            flags[identifier] = step; // update barrier to signal completion of step[step].
+            while (flags[0] < step); // wait for oldest to rewrite arrays.
+        } //proceed to next step.
+    }
+}
 
 void Usage(char* s) {
     fprintf(stderr, "Usage: %s num_elements num_cores input_file output_file");
